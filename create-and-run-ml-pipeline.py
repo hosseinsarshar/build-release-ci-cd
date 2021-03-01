199974deb312be52a2ded93b57f695c7dd9e09e7
Hossein Sarshar <hossein.sarshar@gmail.com>
2021-03-01 12:55:45 -0500

    Test commit

--- create-and-run-ml-pipeline.py
+++ create-and-run-ml-pipeline.py
@@ -17,8 +17,6 @@
 
 # Like always we import some packages related to the Azure ML:
 
-
-
 import azureml.core
 from azureml.core import Workspace, Experiment, Datastore
 from azureml.core.compute import AmlCompute
